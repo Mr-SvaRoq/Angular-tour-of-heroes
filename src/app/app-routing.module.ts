9e5ad29135985f1c57367a18926e630395ecc601
Dinh Nguyen Tien <bibo.dnt.159@gmail.com>
2019-06-11 08:37:11 +0200

    Routing Done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, //redirektuje pathu do ....
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent } //The colon (:) in the path indicates that :id is a placeholder for a specific hero id.
+];
+
+
+@NgModule({
+  // imports: [
+  //   CommonModule
+  // ],
+  // declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
